39a54957e8752fae4be8228c766581c852a57901
milanakdj <58652166+milanakdj@users.noreply.github.com>
2025-01-18 19:44:45 +0545

    Add files via upload
    can convert the audio to nepali text, in nepali itself not in English text

--- main.py
+++ main.py
@@ -29,15 +29,26 @@
 #     print("FFmpeg returned an error:", e)
 
 import whisper
-FILE_LOCATION = "./audio/ENG AND NEPALI 1.m4a"
+# FILE_LOCATION = "./audio/ENG AND NEPALI 1.m4a"
 
 model = whisper.load_model("turbo")
 # result = model.transcribe(r"C:/Documents/projects/audio to script/audio/test.m4a")
 
-task = "translate" # Default is "transcribe"
+task = "transcribe" # Default is "transcribe"
 source_language = "Nepali"
 
+audio_file = "C:/Documents/projects/audio to script/Record (online-voice-recorder.com).mp3"
 # Simply pass "translate" into your task parameter.
-result = model.transcribe("C:/Documents/projects/audio to script/Record (online-voice-recorder.com) (1).mp3", task=task, fp16=False, language=source_language)
+result = model.transcribe(audio_file, task=task, fp16=False, language=source_language)
 
 print(result["text"])
+
+# def translate(audio_file):
+#     options = dict(beam_size=5, best_of=5)
+#     translate_options = dict(task="translate",fp16=False,**options)
+#     result = model.transcribe(audio_file,**translate_options)
+#     return result
+
+# result = translate(audio_file)
+# print(result["text"])
+
