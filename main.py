696b14b2a0dddacb771885cf186ce5569d244649
milanakdj <milanakdj@gmail.com>
2025-01-19 07:33:25 +0000

    can create subtitle, need to change hardcoded values

--- main.py
+++ main.py
@@ -29,26 +29,40 @@
 #     print("FFmpeg returned an error:", e)
 
 import whisper
+import os
 # FILE_LOCATION = "./audio/ENG AND NEPALI 1.m4a"
 
-model = whisper.load_model("turbo")
+model = whisper.load_model("tiny")
+audio_file = "C:/Documents/projects/audio to script/Record (online-voice-recorder.com).mp3"
 # result = model.transcribe(r"C:/Documents/projects/audio to script/audio/test.m4a")
 
-task = "transcribe" # Default is "transcribe"
+# task = "transcribe" # Default is "transcribe"
 source_language = "Nepali"
 
-audio_file = "C:/Documents/projects/audio to script/Record (online-voice-recorder.com).mp3"
 # Simply pass "translate" into your task parameter.
-result = model.transcribe(audio_file, task=task, fp16=False, language=source_language)
+# result = model.transcribe(audio_file, task=task, fp16=False, language=source_language)
 
-print(result["text"])
-
-# def translate(audio_file):
-#     options = dict(beam_size=5, best_of=5)
-#     translate_options = dict(task="translate",fp16=False,**options)
-#     result = model.transcribe(audio_file,**translate_options)
-#     return result
-
-# result = translate(audio_file)
 # print(result["text"])
+
+def translate(audio_file):
+    options = dict(beam_size=5, best_of=5)
+    translate_options = dict(task="translate",fp16=False,**options, language=source_language)
+    result = model.transcribe(audio_file,**translate_options)
+    return result
+
+results = translate(audio_file)
+print(results["text"])
+print(results["segments"])
+
+
+from whisper.utils import WriteVTT
+
+output_dir = "./content"
+audio_path = "test1234.vtt"
+wt = WriteVTT(output_dir="./content")
+
+with open(os.path.join(output_dir,audio_path), "w") as vtt:
+    wt.write_result(result = results, file=vtt)
+
+subtilte = audio_path + ".vtt"
 
